9565dd172ce1bba52c6a6fff0d714bb08e36ed83
Your Name <you@example.com>
2024-03-27 19:52:55 +0300

    add routing

--- src/app.component.js
+++ src/app.component.js
@@ -1,6 +1,12 @@
 import { Component } from "./core/Component";
 import template from "./app.template.hbs";
 
+import './core/Router';
+
+import './pages/home/home.component'
+import './pages/not-found/not-found.component'
+import './pages/board/board.component'
+
 export class App extends Component {
   constructor() {
     super();
