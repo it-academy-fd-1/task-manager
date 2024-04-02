3c7df390b49b7494928f4f2b7a43797f415fada3
Aliaksandr <MY_NAME@example.com>
2024-04-02 18:07:47 +0300

    add components

--- src/pages/sign-in/sign-in.component.js
+++ src/pages/sign-in/sign-in.component.js
@@ -1,11 +1,17 @@
 import { Component } from "../../core/Component";
 import template from "./sign-in.template.hbs";
+import { ROUTES } from "../../constants/routes";
+
+import "../../components/input/input.component";
+import "../../components/button/button.component";
 
 export class SignIn extends Component {
   constructor() {
     super();
 
-    this.template = template();
+    this.template = template({
+      routes: ROUTES,
+    });
   }
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       gridTemplateRows: {
         layout: "max-content 1fr max-content",
       },
+      backgroundImage: {
+        workspace:
+          'url("https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80")',
+      },
     },
   },
   plugins: [require("tailwindcss-animated")],
