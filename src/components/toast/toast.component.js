8cb886142249edc5bfb8bc36b0d0583c9c7abc03
Aliaksandr <MY_NAME@example.com>
2024-04-02 20:21:27 +0300

    complete toast notification

--- src/components/toast/toast.component.js
+++ src/components/toast/toast.component.js
@@ -3,20 +3,23 @@ import { Component } from "../../core/Component";
 import { eventEmitter } from "../../core/EventEmitter";
 import { EVENT_TYPES } from "../../constants/eventTypes";
 import { getToastType } from "./utils";
+import { INITIAL_STATE } from "./initialState";
 
 export class Toast extends Component {
   constructor() {
     super();
 
+    this.timerID = null;
     this.template = template();
-    this.state = {
-      isShow: false,
-      type: null,
-      message: "",
-    };
+    this.state = INITIAL_STATE;
   }
 
+  hideToast = () => {
+    this.setState(INITIAL_STATE);
+  };
+
   showToast = ({ detail }) => {
+    this.timerID = setTimeout(this.hideToast, 3000);
     this.setState({
       isShow: true,
       type: getToastType(detail.type),
@@ -26,10 +29,13 @@ export class Toast extends Component {
 
   componentDidMount() {
     eventEmitter.on(EVENT_TYPES.toastNotification, this.showToast);
+    this.addEventListener("click", this.hideToast);
   }
 
   componentWillUnmount() {
     eventEmitter.off(EVENT_TYPES.toastNotification, this.showToast);
+    this.removeEventListener("click", this.hideToast());
+    clearTimeout(this.timerID);
   }
 }
 
