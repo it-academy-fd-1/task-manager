b28e041c7defcc2b5b3ecb5400dc1b067d7a0b57
Your Name <you@example.com>
2024-04-03 19:15:27 +0300

    add sign in

--- src/pages/sign-in/sign-in.component.js
+++ src/pages/sign-in/sign-in.component.js
@@ -1,6 +1,11 @@
 import { Component } from "../../core/Component";
 import template from "./sign-in.template.hbs";
 import { ROUTES } from "../../constants/routes";
+import { extractFormData } from "../../utils/extractFormData";
+import { authService } from "../../services/Auth";
+import { useToastNotification } from "../../hooks/useToastNotification";
+import { useNavigate } from "../../hooks/useNavigate";
+import { TOAST_TYPE } from "../../constants/toast";
 
 export class SignIn extends Component {
   constructor() {
@@ -16,6 +21,42 @@ export class SignIn extends Component {
     this.template = template({
       routes: ROUTES,
     });
+  }
+
+  toggleIsLoading = () => {
+    this.setState({
+      ...this.state,
+      isLoading: !this.state.isLoading,
+    });
+  };
+
+  signInUser = (evt) => {
+    evt.preventDefault();
+    const formData = extractFormData(evt.target);
+    this.toggleIsLoading();
+    authService
+      .signIn(formData.email, formData.password)
+      .then((data) => {
+        useToastNotification({
+          message: "Success!!!",
+          type: TOAST_TYPE.success,
+        });
+        useNavigate(ROUTES.dashboard);
+      })
+      .catch((error) => {
+        useToastNotification({ message: error.message });
+      })
+      .finally(() => {
+        this.toggleIsLoading();
+      });
+  };
+
+  componentDidMount() {
+    this.addEventListener("submit", this.signInUser);
+  }
+
+  componentWillUnmount() {
+    this.removeEventListener("submit", this.signInUser);
   }
 }
 
