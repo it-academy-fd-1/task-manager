1b560cd779ebd78deb1615e802bf681815ac4c4f
Aliaksandr <MY_NAME@example.com>
2024-04-18 21:07:34 +0300

    add attachments

--- src/app.component.js
+++ src/app.component.js
@@ -21,6 +21,7 @@ import "./components/button/button.component";
 import "./components/loader/loader.component";
 import "./components/modal/modal.component";
 import "./components/create-board-form/create-board-form-components";
+import "./components/create-task-form/create-task-form.component";
 import "./components/task-card/task-card.component";
 
 export class App extends Component {
@@ -47,7 +48,6 @@ export class App extends Component {
     authService
       .authorizeUser()
       .then((user) => {
-        console.log(user);
         if (user.uid) {
           setUser({ ...user });
         }
--- src/pages/board/board.component.js
+++ src/pages/board/board.component.js
@@ -2,6 +2,9 @@ import { Component } from "../../core/Component";
 import template from "./board.template.hbs";
 import { useUserStore } from "../../hooks/useUserStore";
 import { INITIAL_STATE } from "./initialState";
+import { useNavigate } from "../../hooks/useNavigate";
+import { ROUTES } from "../../constants/routes";
+import { useModal } from "../../hooks/useModal";
 
 export class BoardPage extends Component {
   constructor() {
@@ -19,8 +22,34 @@ export class BoardPage extends Component {
     });
   }
 
+  openCreateTaskModal = () => {
+    useModal({
+      isOpen: true,
+      title: "Create Task",
+      successCaption: "Create",
+      template: "ui-create-task-form",
+      onSuccess: () => {
+        console.log("Success!!!");
+      },
+    });
+  };
+
+  onClick = ({ target }) => {
+    const goToDashboard = target.closest(".go-to-dashboard");
+    const createTaskBtn = target.closest(".create-task-btn");
+
+    if (goToDashboard) {
+      useNavigate(ROUTES.dashboard);
+    }
+
+    if (createTaskBtn) {
+      this.openCreateTaskModal();
+    }
+  };
+
   componentDidMount() {
     this.initialization();
+    this.addEventListener("click", this.onClick);
   }
 }
 
