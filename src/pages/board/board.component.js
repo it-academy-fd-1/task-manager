d2eb93e2f73123678a042d1ef919102a62cfed22
Aliaksandr <MY_NAME@example.com>
2024-04-23 20:31:41 +0300

    add loading attachments

--- src/pages/board/board.component.js
+++ src/pages/board/board.component.js
@@ -5,6 +5,8 @@ import { INITIAL_STATE } from "./initialState";
 import { useNavigate } from "../../hooks/useNavigate";
 import { ROUTES } from "../../constants/routes";
 import { useModal } from "../../hooks/useModal";
+import { extractFormData } from "../../utils/extractFormData";
+import { storageService } from "../../services/Storage";
 
 export class BoardPage extends Component {
   constructor() {
@@ -22,14 +24,41 @@ export class BoardPage extends Component {
     });
   }
 
+  uploadAttachments(attachments) {
+    const { user, boardId } = this.state;
+    const path = `${user.uid}/${boardId}`;
+    const promiseFiles = attachments.map((attachment) => {
+      return storageService.uploadFile(attachment, path);
+    });
+
+    return Promise.all(promiseFiles);
+  }
+
+  loadAttachmentsUrl(data) {
+    return Promise.all(
+      data.map((snapshot) => storageService.downloadURL(snapshot.ref))
+    );
+  }
+
   openCreateTaskModal = () => {
     useModal({
       isOpen: true,
       title: "Create Task",
       successCaption: "Create",
       template: "ui-create-task-form",
-      onSuccess: () => {
-        console.log("Success!!!");
+      onSuccess: (modal) => {
+        const form = modal.querySelector(".create-task-form");
+        const formData = new FormData(form);
+        const preparedData = {
+          ...extractFormData(form),
+          attachments: formData.getAll("attachments"),
+        };
+
+        this.uploadAttachments(preparedData.attachments)
+          .then(this.loadAttachmentsUrl)
+          .then((data) => {
+            console.log(data);
+          });
       },
     });
   };
